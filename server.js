0a3bdf419dce084c0ca9f6d75697f29732387ed8
Caleb Harshman <caleb.harsh139@gmail.com>
2018-04-03 14:01:24 -0700

    added things to production

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const bodyParser = require("body-parser")
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -19,6 +15,14 @@ const routes = require("./routes/apiRoutes.js")
 // Define any API routes before this runs
 app.use(routes)
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
